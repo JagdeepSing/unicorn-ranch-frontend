dd08f02108e97f761c9c920da9f2b20aae091e03
Jag <jagdeepsingh3939@gmail.com>
2019-08-01 19:49:57 -0700

    converted unicorn area and card to components

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import './App.css';
 
-class App extends React.Component {
+export default class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
@@ -13,15 +13,16 @@ class App extends React.Component {
   }
 
   async getUnicorns() {
-    const unicorns = await fetch('http://localhost:3001/unicorns').then(response => response.json());
+    const unicorns = await fetch('http://localhost:3001/unicorns').then((response) => response.json());
+
     const barn = [];
     const pasture = [];
     const trails = [];
 
-    unicorns.forEach(unicorn => {
-      if (unicorn.location === 'barn') {
+    unicorns.forEach((unicorn) => {
+      if (unicorn.location.toLowerCase() === 'barn') {
         barn.push(unicorn);
-      } else if (unicorn.location === 'pasture') {
+      } else if (unicorn.location.toLowerCase() === 'pasture') {
         pasture.push(unicorn);
       } else {
         trails.push(unicorn);
@@ -31,52 +32,42 @@ class App extends React.Component {
     this.setState({
       barnUnicorns: barn,
       pastureUnicorns: pasture,
-      trailsUnicrons: trails,
+      trailsUnicorns: trails,
     });
   }
 
   render() {
     return (
       <>
-        <section id="barn">
-          <h2>Barn</h2>
-          <ul>
-            {
-              this.state.barnUnicorns.map((unicorn) => {
-                return (
-                  <li>
-                    <div>
-                      <h4>{unicorn.name}</h4>
-                      <p>{unicorn.location}</p>
-                      <p>{unicorn.favorite_food}</p>
-                    </div>
-                  </li>
-                  );
-              })
-            }
-          </ul>
-        </section>
-
-        <section id="pasture">
-          <h2>Pasture</h2>
-          <ul>
-            <li>
-              Green Unicorn
-            </li>
-          </ul>
-        </section>
-        
-        <section id="trails">
-          <h2>Trails</h2>
-          <ul>
-            <li>
-              Purple Unicorn
-            </li>
-          </ul>
-        </section>
+        <UnicornArea id="barn" heading="Barn" unicorns={this.state.barnUnicorns} />
+        <UnicornArea id="pasture" heading="Pasture" unicorns={this.state.pastureUnicorns} />
+        <UnicornArea id="trails" heading="Trails" unicorns={this.state.trailsUnicorns} />
       </>
     );
   }
 }
 
-export default App;
+class UnicornCard extends React.Component {
+  render() {
+    return (
+      <div id={this.props.unicorn.id} className="unicorn">
+        <h4>{this.props.unicorn.name}</h4>
+        <p>{this.props.unicorn.color}</p>
+        <p>{this.props.unicorn.favorite_food}</p>
+      </div>
+    );
+  }
+}
+
+class UnicornArea extends React.Component {
+  render() {
+    return (
+      <section id={this.props.id}>
+        <h2>{this.props.heading}</h2>
+        {this.props.unicorns.map((unicorn) => {
+          return <UnicornCard key={unicorn.id} unicorn={unicorn} />;
+        })}
+      </section>
+    );
+  }
+}
