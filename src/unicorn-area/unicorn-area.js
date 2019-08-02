86556fb50dd8c438f828e2d3106da4917d8c7194
Jag <jagdeepsingh3939@gmail.com>
2019-08-01 21:32:21 -0700

    added minimal styling

--- src/unicorn-area/unicorn-area.js
+++ src/unicorn-area/unicorn-area.js
@@ -1,16 +1,19 @@
 import React from 'react';
 
 import UnicornCard from '../unicorn-card/unicorn-card';
+import './unicorn-area.css';
 
 export default class UnicornArea extends React.Component {
   render() {
     return (
-      <section id={this.props.id}>
-        <h2>{this.props.heading}</h2>
+      <>
+      <h2>{this.props.heading}</h2>
+      <section id={this.props.id} className="unicorn-area">
         {this.props.unicorns.map((unicorn) => {
           return <UnicornCard key={unicorn.id} unicorn={unicorn} moveUnicorn={this.props.moveUnicorn} />;
         })}
       </section>
+      </>
     );
   }
 }
--- src/unicorn-card/unicorn-card.js
+++ src/unicorn-card/unicorn-card.js
@@ -1,16 +1,24 @@
 import React from 'react';
 
+import './unicorn-card.css';
+
 export default class UnicornCard extends React.Component {
   render() {
     return (
-      <div id={this.props.unicorn.id} className="unicorn">
+      <div id={this.props.unicorn.id} className="unicorn-card">
         <h4>{this.props.unicorn.name}</h4>
-        <p>{this.props.unicorn.color}</p>
-        <p>{this.props.unicorn.favorite_food}</p>
+        <p>Color: {this.props.unicorn.color}</p>
+        <p>Favorite Food: {this.props.unicorn.favorite_food}</p>
 
-        <button onClick={() => this.props.moveUnicorn(this.props.unicorn, 'barn')}>Move to Barn</button>
-        <button onClick={() => this.props.moveUnicorn(this.props.unicorn, 'pasture')}>Move to Pasture</button>
-        <button onClick={() => this.props.moveUnicorn(this.props.unicorn, 'trails')}>Move to Trails</button>
+        {this.props.unicorn.location !== 'barn' && 
+          <button onClick={() => this.props.moveUnicorn(this.props.unicorn, 'barn')}>Move to Barn</button>
+        }
+        {this.props.unicorn.location !== 'pasture' &&
+          <button onClick={() => this.props.moveUnicorn(this.props.unicorn, 'pasture')}>Move to Pasture</button>
+        }
+        {this.props.unicorn.location !== 'trails' && 
+          <button onClick={() => this.props.moveUnicorn(this.props.unicorn, 'trails')}>Move to Trails</button>
+        }
       </div>
     );
   }
